ef863f9ef4e9d055ff0ba18e48520b1f4787071a
Manuel Bärenz <turion@enigma.(none)>
2010-06-16 17:30:07 +0200

    Initialisation of git control over enigmage

---
+++ enigmage-viewer.py
@@ -0,0 +1,153 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+enigmage-viewer.py
+ 
+Das eigentliche Programm
+
+Bereits geschafft:
+1.Sprites ausprobieren
+	Event loop, verwende gescheites timing
+2.Bilder verschieben
+3a Mage
+	Bewegung flüssig mit target und friction
+	Zoom in/Zoom out bzw. Fullscreen/Thumb
+3b. Mehrere Bilder gleichzeitig
+	Eigene Gruppe
+4z. Nodes statt:
+	Virtuelle Verzeichnisse, MageDirectory Klasse
+		aktives Verzeichnis mit Grundfunktionen zur Kommunikation mit einer Bildschirmgruppe
+
+Aktuell:
+
+4a. Dateiverwaltung
+	Via Nodes...
+		Unterklassen davon, die tatsächlich etwas können, z.B. SQL, Dateisystem
+		Selbstständiger DirectoryNode
+		Davon eine Unterklasse, die automatisch die Mages lädt?
+	os.path verwenden
+
+Noch zu tun:
+4a. Dateiverwaltung
+4b. SQL
+		Tags
+			Nicht nur Baumstruktur, sondern Mitgliedschaft in mehreren Gruppen via hierarchischer Tags
+			Hierarchie der Tags über tag_hierarchy ist mengenartig (ein Tag kann mehrere Übertags haben) und ausschließlich direkt (nicht transitiv: Tag3 Übertag für Tag2 und Tag 2 Übertag für Tag1 führt nicht dazu, dass Tag3 Übertag für Tag1. Man kann diese Struktur aber trotzdem baumartig benutzen.)
+			Tagzugehörigkeit wird durch Coupling quantifiziert
+				Rating geschieht durch Tags (z.B. tag "kalenderhaft", "gut", "überbelichtet") und coupling
+
+5. Alles zusammenstecken
+		Gedanken wegen Packages machen, vielleicht main() implementieren
+6. Praktische Funktionen einbauen/ Eyecandy
+		"Einsortieren": Zwei Gruppen, eine links im halben Fullscreen die neuen Fotos durchblätternd, die rechts in einen Baum einsortiert werden
+		MageLabelled
+		LayeredUpdates anstatt Group verwenden um Überlappungen in den Griff zu bekommen
+		Baumartige Gruppen
+		Dateien nicht nur lesen, sondern auch schreiben
+7. Performance verbessern
+		Mages nur einmal laden, Überprüfung durch Gruppenzugehörigkeit
+		Bilder außerhalb der Sichtbereiches nicht zeichnen
+		DirtyRects
+		done
+		Multithreading
+			Hintergrundprozess, der Thumbs erstellt und Bilder vorlädt usw.
+8. OpenGL-Backend
+"""
+
+import enigmage, enigtree, enigmage.directory
+
+import os, sys, pygame, pygame.sprite
+
+pygame.init()
+
+size = 800, 600
+maxbildversize = 300
+screen = pygame.display.set_mode(size)
+
+enigmage.init(screen)
+
+os.chdir('/')
+dir = '/home/turion/Fotos/selection enigmage'
+scrambled_eggs = enigmage.directory.MageDirNode(dir)
+meinesprites = enigmage.Mages(screen.get_rect(), scrambled_eggs)
+
+
+#bildname = "/home/turion/Fotos/2008.01.02.1007.Letzter Foto.JPG"
+#zaunkoenig = pygame.image.load(bildname).convert()
+#zaunsprite = enigmage.Mage(zaunkoenig, title="Zaunkönig")
+#zaunnode = enigtree.Node(zaunsprite)
+#
+#bildname = "/home/turion/Fotos/Best of/2008.01.01.0957.JPG"
+#glas = pygame.image.load(bildname).convert()
+#glassprite = enigmage.Mage(glas, title="Autoscheibe")
+#glasnode = enigtree.Node(glassprite, parent=zaunnode)
+#
+#bildname = "/home/turion/Fotos/Best of/2008.03.15.2959.JPG"
+#tobi = pygame.image.load(bildname).convert()
+#tobisprite = enigmage.Mage(tobi, title="Tobias")
+#tobinode = enigtree.Node(tobisprite, parent=zaunnode)
+#
+#bildname = "/home/turion/Fotos/Best of/2008.01.16.1375.JPG"
+#mond = pygame.image.load(bildname).convert()
+#mondsprite = enigmage.Mage(mond, title="Mondfinsternis")
+#mondnode = enigtree.Node(mondsprite, parent=tobinode)
+#
+#bildname = "/home/turion/Fotos/Best of/2008.03.15.2962.JPG"
+#fern = pygame.image.load(bildname).convert()
+#fernsprite = enigmage.Mage(fern, title="Fernglas")
+#fernnode = enigtree.Node(fernsprite, parent=tobinode)
+#
+#bildname = "/home/turion/Fotos/Best of/2008.04.05.3849.JPG"
+#miri = pygame.image.load(bildname).convert()
+#mirisprite = enigmage.Mage(miri, title="Miriam")
+#mirinode = enigtree.Node(mirisprite, parent = mondnode)
+
+#meinesprites = enigmage.Mages(screen.get_rect(), zaunnode)
+
+
+#def load_mage_node(path, title):
+	#image = pygame.image.load(path).convert()
+	#sprite = enigmage.Mage(image, title=title)
+	#node = enigtree.Node(sprite)
+	#return node
+	#
+#root = load_mage_node('/home/turion/Fotos/selection kalender/internet mirror/2007.07.01.0583_sm.jpg', 'Flügel')
+#mauer = load_mage_node('/home/turion/Fotos/selection kalender/internet mirror/2007.07.05.0818_sm.jpg', 'Mauer')
+#mauer.parent = root
+#kreuz = load_mage_node('/home/turion/Fotos/selection kalender/internet mirror/2007.07.07.0895_sm.jpg', 'Kreuz')
+#kreuz.parent = root
+#lampen = load_mage_node('/home/turion/Fotos/selection kalender/internet mirror/2007.07.07.0902_sm.jpg', 'Lampen')
+#lampen.parent = mauer
+#tempel = load_mage_node('/home/turion/Fotos/selection kalender/internet mirror/2007.07.07.0906_sm.jpg', 'Tempel')
+#tempel.parent = kreuz
+#panzer = load_mage_node('/home/turion/Fotos/selection kalender/internet mirror/2007.07.07.0926_sm.jpg', 'Panzer')
+#panzer.parent = kreuz
+#himmel = load_mage_node('/home/turion/Fotos/selection kalender/internet mirror/2007.07.13.1026_sm.jpg', 'Himmel')
+#himmel.parent = panzer
+ #= load_mage_node('/home/turion/Fotos/selection kalender/internet mirror/', '')
+ #= load_mage_node('/home/turion/Fotos/selection kalender/internet mirror/', '')
+ #= load_mage_node('/home/turion/Fotos/selection kalender/internet mirror/', '')
+ #= load_mage_node('/home/turion/Fotos/selection kalender/internet mirror/', '')
+
+#meinesprites = enigmage.Mages(screen.get_rect(), root)
+
+enigmage.var.tick()
+loopcount = 0
+while True:
+	enigmage.var.tick()
+	if enigmage.var.time > 1000: print "Delay in loop ", loopcount, ":", enigmage.var.time
+	loopcount += 1
+	for event in pygame.event.get():
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_ESCAPE: sys.exit()
+			if event.key == pygame.K_RIGHT: meinesprites.focus_right()
+			if event.key == pygame.K_LEFT: meinesprites.focus_left()
+			if event.key == pygame.K_DOWN: meinesprites.zoom_in()
+			if event.key == pygame.K_UP: meinesprites.zoom_out()
+	# events.pump oder so?
+	keys = pygame.key.get_pressed()
+	meinesprites.clear(enigmage.var.screen,enigmage.var.background)
+	meinesprites.update()
+	meinesprites.draw(enigmage.var.screen) # dirtyrects = meinesprites.draw(var.screen)
+	pygame.display.flip() # pygame.display.update(dirtyrects)
