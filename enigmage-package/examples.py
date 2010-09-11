38cc7f28494fafc56ed9924c9e1df4d90cf3c483
Manuel Bärenz <manuel@enigmage.de>
2010-09-11 18:21:54 +0200

    Physics was bottleneck. Now runs on lower resolution, thus much faster.

--- enigmage-package/examples.py
+++ enigmage-package/examples.py
@@ -6,12 +6,15 @@
 import enigmage, pygame
 
 loop = True
+debug_clock = pygame.time.Clock()
+
 def main_loop(mages_list, keyactions):
 	enigmage.tick()
 	loopcount = 0
 	global loop
 	while loop:
 		enigmage.tick()
+		print "Ticked: ", debug_clock.tick()
 		if enigmage.time > 100:
 			print "Delay in loop ", loopcount, ":", enigmage.time, "milliseconds"
 		loopcount += 1
@@ -23,11 +26,16 @@ def main_loop(mages_list, keyactions):
 					pass
 		# events.pump oder so?
 		# keys = pygame.key.get_pressed() gibts auch
+		print "Keys: ", debug_clock.tick()
 		for mages in mages_list:
 			mages.clear(enigmage.screen,enigmage.background)
+			print "Cleared: ", debug_clock.tick()
 			mages.update()
+			print "Updated: ", debug_clock.tick()
 			mages.draw(enigmage.screen) # dirtyrects = mages.draw(var.screen)
+			print "Drawed: ", debug_clock.tick()
 		pygame.display.flip() # pygame.display.update(dirtyrects)
+		print "Flipped: ", debug_clock.tick()
 	enigmage.exit()
 
 def end_main_loop():
